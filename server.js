fcc45457d07ae23e6114b3c139e2800281c6b442
= <=>
2013-06-02 20:53:46 +0100

    changed port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // note, io.listen(<port>) will create a http server for you
-var io = require('socket.io').listen(8080);
+var io = require('socket.io').listen(80);
 
 io.sockets.on('connection', function (socket) {
   	socket.on('speak', function (data) {

60b06b8605c91b2f9ed05a1fc70b7ed336491d67
= <=>
2013-06-02 20:45:07 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+// note, io.listen(<port>) will create a http server for you
+var io = require('socket.io').listen(8080);
+
+io.sockets.on('connection', function (socket) {
+  	socket.on('speak', function (data) {
+    	io.sockets.emit('speak', data);
+  	});
+  	
+  	socket.on('disconnect', function () {
+    	io.sockets.emit('user disconnected');
+  	});
+});
